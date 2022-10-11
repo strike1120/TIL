78c9a0fcd1286244ee78baeaed10565f87c2ad83
CHO SUNGHO <70555626+strike1120@users.noreply.github.com>
2022-10-11 11:01:11 +0900

    Update practice_11.java

--- Day_1007/practice_11.java
+++ Day_1007/practice_11.java
@@ -10,31 +10,32 @@ import java.util.Scanner;
 public class practice_11 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a,b,c, temp;
+		System.out.print("첫 번째 수: ");
+		a = sc.nextInt();
+		System.out.print("세 번째 수: ");
+		b = sc.nextInt();
+		System.out.print("두 번째 수: ");
+		c = sc.nextInt();
 		
-		Scanner scanner = new Scanner(System.in);
+		//두 수를 비교하여 a가 b보다 큰 값을 갖도록 정렬
+		//Compare two numbers and sort so that a is greater than b
+		temp = (b > a) ? a : 0; //a,b둘중 작은 값을 temp에 넣는다
+		a = (temp != 0)? b : a;
+		b = (temp !=0) ? temp : b;
 		
-		int num1,num2,num3;
-		int max, min, mid;
+		temp = 0;//temp값을 0으로 초기화
+		temp = (c > a) ? a : 0; //a,c둘 중 더 작은 값을 temp에 넣는다
+		a = (temp != 0)? c : a;
+		c = (temp !=0) ? temp : c;
 		
-		num1 = scanner.nextInt();
-		num2 = scanner.nextInt();
-		num3 = scanner.nextInt();
+		temp = 0;//temp값을 0으로 초기화
+		temp = (c > b) ? b : 0; //a,c둘 중 더 작은 값을 temp에 넣는다
+		b = (temp != 0)? c : b;
+		c = (temp !=0) ? temp : c;
 		
-		(num1>num2)&&(num1>num3)? max=num1 :"";
-		(num2>num1)&&(num2>num3)? max=num2 :"";
-		(num3>num1)&&(num3>num2)? max=num3 :"";
-		
-		(num1<num2)&&(num1<num3)? min=num1 :"";
-		(num2<num1)&&(num2<num3)? min=num2 :"";
-		(num3<num1)&&(num3<num2)? min=num3 :"";
-
-		(num1<num2)&&(num1>num3)? mid=num1 :"";
-		(num2<num1)&&(num2>num3)? mid=num2 :"";
-		(num3<num1)&&(num3>num2)? mid=num3 :"";
-		
-		
-		System.out.println(max,mid,min);
-		
+		System.out.println(a+","+ b +"," + c);
 	}
 
 }
